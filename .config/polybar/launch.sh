e5c9cb9c649198f03647a4d972a59d833a7710e9
Ravieroy <ravieroy123@gmail.com>
2024-12-09 14:56:31 +0530

    add settings for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,10 +9,17 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch polybar
 polybar main -c $(dirname "$0")/config.ini &
 
-if [[ $(xrandr -q | grep 'DisplayPort-1') ]]; then
-	polybar external -c $(dirname $0)/config.ini &
-fi
+##--- Uncomment this when you connect the external monitor via Display Port
+# if [[ $(xrandr -q | grep 'DisplayPort-1') ]]; then
+# 	polybar external -c $(dirname $0)/config.ini &
+# fi
 
+##--- Uncomment this when you connect the vertical monitor into HDMI slot
+# if [[ $(xrandr -q | grep 'HDMI-A-0') ]]; then
+# 	polybar vertical -c $(dirname $0)/config.ini &
+# fi
+
+##--- Uncomment this when you only use the external monitor in the HDMI slot
 if [[ $(xrandr -q | grep 'HDMI-A-0') ]]; then
-	polybar vertical -c $(dirname $0)/config.ini &
+	polybar external -c $(dirname $0)/config.ini &
 fi
